3996b6f524adcc0a396cd102b023263d4f94ae68
yurir <yuri1992@gmail.com>
2017-09-08 16:17:14 +0300

    Extend class to have information about the attcked point

--- src/game/engine/GameTurn.java
+++ src/game/engine/GameTurn.java
@@ -1,11 +1,15 @@
 package game.engine;
 
+import game.runners.console.ConsoleUtils;
+import game.ships.ShipPoint;
+
 import java.util.Date;
 
 public class GameTurn {
     private Date startAt;
     private Date endAt = null;
     private boolean isHit = false;
+    private ShipPoint point;
 
     public GameTurn() {
         startAt = new Date();
@@ -25,5 +29,13 @@ public class GameTurn {
 
     public void setHit(boolean hit) {
         isHit = hit;
+    }
+
+    public void setPoint(ShipPoint point) {
+        this.point = point;
+    }
+
+    public String toString() {
+        return String.format((isHit ? "Hit" : "Miss") + " on " + point.toString() + " took " + ConsoleUtils.formatDateHM(getTurnTime()));
     }
 }
