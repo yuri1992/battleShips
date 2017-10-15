d0cb0b919ad27f9f65185219bffb8ad2484fafc7
yurir <yuri1992@gmail.com>
2017-10-15 21:18:23 +0300

    Create Common Servlet for json and base

--- BattleShipsWebApp/src/servlets/BaseServlet.java
+++ BattleShipsWebApp/src/servlets/BaseServlet.java
@@ -4,41 +4,12 @@ import com.google.gson.JsonObject;
 import engine.model.multi.User;
 import utils.SessionUtils;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by amirshavit on 10/15/17.
- */
-public abstract class BaseServlet extends HttpServlet{
-
-    abstract protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException;
-
-    //<editor-fold defaultstate="collapsed" desc="HttpServlet Overrides"
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json;charset=UTF-8");
-        processRequest(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json;charset=UTF-8");
-        processRequest(req, resp);
-    }
-
-    @Override
-    public String getServletInfo() {
-
-        return this.getClass().toString();
-    }
-
-    //</editor-fold>
-
+public abstract class BaseServlet extends HttpServlet {
     protected boolean isSessionValid(HttpServletRequest request, HttpServletResponse response) throws IOException {
         User user = SessionUtils.getSessionUser(request);
         if (user == null) {
@@ -51,5 +22,4 @@ public abstract class BaseServlet extends HttpServlet{
         }
         return true;
     }
-
 }
---
+++ BattleShipsWebApp/src/servlets/JsonServlet.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by amirshavit on 10/15/17.
+ */
+public abstract class JsonServlet extends BaseServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json;charset=UTF-8");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json;charset=UTF-8");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return this.getClass().toString();
+    }
+
+
+}
