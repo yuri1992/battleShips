5b333698be47f440a7bb1e74b89a9d4020d0feae
Amir Shavit <amir.shavit@autodesk.com>
2017-10-18 00:07:17 +0300

    parse game api path to action types

--- BattleShipsWebApp/src/servlets/APIGameServlet.java
+++ BattleShipsWebApp/src/servlets/APIGameServlet.java
@@ -12,6 +12,54 @@ import java.io.IOException;
 
 public class APIGameServlet extends JsonServlet  {
 
+    private enum APIGamePathTypes {
+        NONE,
+        STATUS,
+        STATISTICS,
+        PLAY
+    }
+
+    // Accommodate several REST requests for same endpoint
+    private class RouteRestRequest {
+        private final String statusPath = "status";
+        private final String statisticsPath = "statistics";
+        private final String playPath = "play";
+
+        private APIGamePathTypes pathType = APIGamePathTypes.NONE;
+
+        private RouteRestRequest(String pathInfoOriginal) throws ServletException {
+
+            // Remove first / if exist
+            String pathInfo = pathInfoOriginal;
+            if (pathInfo != null && pathInfo.startsWith("/"))
+                pathInfo = pathInfo.substring(1);
+            String[] tokens = (pathInfo != null ? pathInfo.split("/") : null);
+
+            if (tokens == null || tokens.length == 0 || tokens[0].isEmpty()) return;
+
+            // Check for ID case first
+            if (tokens.length == 1) {
+                // check 2nd token
+                if (statusPath.equals(tokens[0]))
+                    pathType = APIGamePathTypes.STATUS;
+                else if (statisticsPath.equals(tokens[0]))
+                    pathType = APIGamePathTypes.STATISTICS;
+                else if (playPath.equals(tokens[0]))
+                    pathType = APIGamePathTypes.PLAY;
+                else
+                    throw new ServletException("Invalid URI");
+            } else {
+                // to many tokens
+                throw new ServletException("Invalid URI");
+            }
+        }
+
+        public APIGamePathTypes getPathType() {
+            return pathType;
+        }
+    }
+
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         /*
             handle move
@@ -24,7 +72,24 @@ public class APIGameServlet extends JsonServlet  {
         super.doGet(request, response);
         if (!isSessionValid(request, response)) return;
 
-        getMatchMetaData(request, response);
+        try {
+            RouteRestRequest route = new RouteRestRequest(request.getPathInfo());
+            switch (route.getPathType()) {
+                case NONE:
+                    getMatchMetaData(request, response);
+                    break;
+                case STATUS:
+                    System.out.println("APIGameServlet.doGet() with status path");
+                    break;
+                case STATISTICS:
+                    System.out.println("APIGameServlet.doGet() with statistics path");
+                    break;
+                default:
+                    setResponseError(response, HttpServletResponse.SC_NOT_FOUND, "Path not supported for GET");
+            }
+        } catch (ServletException e) {
+            setResponseError(response, HttpServletResponse.SC_NOT_FOUND, "Invalid request");
+        }
     }
 
     private void getMatchMetaData(HttpServletRequest request, HttpServletResponse response) throws IOException {
