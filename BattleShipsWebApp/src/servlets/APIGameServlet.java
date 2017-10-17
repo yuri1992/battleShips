8607c5ee34fca699d31f840910631209d450612c
Amir Shavit <amir.shavit@autodesk.com>
2017-10-17 22:22:12 +0300

    Add GameServlet

--- BattleShipsWebApp/src/servlets/APIGameServlet.java
+++ BattleShipsWebApp/src/servlets/APIGameServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class APIGamePlayServlet extends JsonServlet  {
+public class APIGameServlet extends JsonServlet  {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         /*
             handle move
