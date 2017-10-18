d3a0d9f351fce79d9b11ca9327400affac32193c
Amir Shavit <amir.shavit@autodesk.com>
2017-10-18 22:21:14 +0300

    GET game statistics - add more data

--- BattleShipsWebApp/src/models/GameStatisticsObj.java
+++ BattleShipsWebApp/src/models/GameStatisticsObj.java
@@ -1,7 +1,12 @@
 package models;
 
 import engine.managers.game.GameStatistics;
+import engine.managers.game.GameTurn;
+import engine.model.boards.Player;
 import engine.model.multi.Match;
+import engine.model.multi.User;
+
+import java.util.List;
 
 /**
  * Created by amirshavit on 10/18/17.
@@ -9,8 +14,21 @@ import engine.model.multi.Match;
 public class GameStatisticsObj {
 
     private final GameStatistics gameStatistics;
+    private List<GameTurn> turnList;
+    private User winner;
 
     public GameStatisticsObj(Match match) {
         this.gameStatistics = match.getGameManager().getGameStatistics();
+        this.turnList = match.getGameManager().getTurnList();
+
+        Player winner = match.getGameManager().getWinner();
+        if (winner != null) {
+            if (match.getGameManager().getPlayerList().indexOf(winner) == 0)
+                this.winner = match.getPlayer1();
+            else
+                this.winner = match.getPlayer2();
+        } else {
+            this.winner = null;
+        }
     }
 }
