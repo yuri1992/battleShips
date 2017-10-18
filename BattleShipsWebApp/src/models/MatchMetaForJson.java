dc845b0fd92e7e2e45d90d5a269cf770771a0d35
Amir Shavit <amir.shavit@autodesk.com>
2017-10-18 08:17:27 +0300

    Add MatchStatus to Match meta data

--- BattleShipsWebApp/src/models/MatchMetaForJson.java
+++ BattleShipsWebApp/src/models/MatchMetaForJson.java
@@ -14,6 +14,7 @@ public class MatchMetaForJson {
     private final UserForJson submittingUser;
     private final UserForJson player1;
     private final UserForJson player2;
+    private final MatchStatus matchStatus;
     private final GameMode gameMode;
     private final int boardSize;
     private final int playersCount;
@@ -24,12 +25,24 @@ public class MatchMetaForJson {
         this.matchId = match.getMatchId();
         this.matchName = match.getMatchName();
         this.submittingUser = new UserForJson(match.getSubmittingUser());
-        this.playersCount = (match.getPlayer1() != null ? 1 : 0) + (match.getPlayer2() != null ? 1 : 0);
         this.player1 = (match.getPlayer1() != null ? new UserForJson(match.getPlayer1()) : null);
         this.player2 = (match.getPlayer2() != null ? new UserForJson(match.getPlayer2()) : null);
         this.gameMode = match.getGameType();
         this.boardSize = match.getBoardSize();
         this.state = match.getGameManager().getState();
+
+        if (this.player1 == null) {
+            this.matchStatus = MatchStatus.WAITING_FOR_FIRST_PLAYER;
+            this.playersCount = 0;
+        }
+        else if (this.player2 == null) {
+            this.matchStatus = MatchStatus.WAITING_FOR_SECOND_PLAYER;
+            this.playersCount = 1;
+        }
+        else {
+            this.matchStatus = MatchStatus.IN_PROGRESS;
+            this.playersCount = 2;
+        }
     }
 
 }
---
+++ BattleShipsWebApp/src/models/MatchStatus.java
@@ -0,0 +1,12 @@
+package models;
+
+/**
+ * Created by amirshavit on 10/18/17.
+ */
+public enum MatchStatus {
+
+    WAITING_FOR_FIRST_PLAYER,
+    WAITING_FOR_SECOND_PLAYER,
+    IN_PROGRESS
+
+}
