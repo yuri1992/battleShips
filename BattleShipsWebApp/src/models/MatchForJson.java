638e74046a8f4a2eb3d0c2ce760a4b32adcaa720
yurir <yuri1992@gmail.com>
2017-10-18 07:57:50 +0300

    Adding Additional Params for games model

--- BattleShipsWebApp/src/models/MatchForJson.java
+++ BattleShipsWebApp/src/models/MatchForJson.java
@@ -1,8 +1,8 @@
 package models;
 
 import engine.managers.game.GameMode;
+import engine.managers.game.GameState;
 import engine.model.multi.Match;
-import engine.model.multi.User;
 
 /**
  * Created by amirshavit on 10/17/17.
@@ -16,16 +16,20 @@ public class MatchForJson {
     private final UserForJson player2;
     private final GameMode gameMode;
     private final int boardSize;
+    private final int playersCount;
+    private final GameState state;
 
     public MatchForJson(Match match) {
 
         this.matchId = match.getMatchId();
         this.matchName = match.getMatchName();
         this.submittingUser = new UserForJson(match.getSubmittingUser());
+        this.playersCount = (match.getPlayer1() != null ? 1 : 0) + (match.getPlayer2() != null ? 1 : 0);
         this.player1 = (match.getPlayer1() != null ? new UserForJson(match.getPlayer1()) : null);
         this.player2 = (match.getPlayer2() != null ? new UserForJson(match.getPlayer2()) : null);
         this.gameMode = match.getGameType();
         this.boardSize = match.getBoardSize();
+        this.state = match.getGameManager().getState();
     }
 
 }
