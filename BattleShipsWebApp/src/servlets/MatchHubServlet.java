9a1a04980dbe4a68dbfa3158db2aa51dbab07df2
yurir <yuri1992@gmail.com>
2017-10-21 19:05:33 +0300

    Player that register to game, need to be redirect to that page.

--- BattleShipsWebApp/src/servlets/MatchHubServlet.java
+++ BattleShipsWebApp/src/servlets/MatchHubServlet.java
@@ -1,5 +1,7 @@
 package servlets;
 
+import engine.managers.game.GameState;
+import engine.model.multi.Match;
 import engine.model.multi.User;
 import utils.SessionUtils;
 
@@ -15,13 +17,21 @@ import java.io.IOException;
 public class MatchHubServlet extends BaseServlet {
 
     private static final String PAGE_SIGNUP_JSP = "/pages/signup";
+    private static final String PAGE_GAME_JSP = "/pages/game";
     private static final String MATCH_HUB_JSP = "matchhub.jsp";
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User user = SessionUtils.getSessionUser(req);
         if (user == null) { // User already logged id in
             resp.sendRedirect(PAGE_SIGNUP_JSP);
+            return;
+        }
+
+        Match match = SessionUtils.getSessionMatch(req);
+        if (match != null && match.getGameManager().getState() != GameState.REPLAY) {
+            resp.sendRedirect(PAGE_GAME_JSP);
             return;
         }
 
