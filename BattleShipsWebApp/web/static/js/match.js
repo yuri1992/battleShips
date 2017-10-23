351165ae2664c282193b3e75b140998cb275a085
yurir <yuri1992@gmail.com>
2017-10-23 08:54:48 +0300

    Game - Average time - display as seconds instead of milisec #57

--- BattleShipsWebApp/web/static/js/match.js
+++ BattleShipsWebApp/web/static/js/match.js
@@ -259,7 +259,7 @@ $(function ($) {
             result.append("<tr><td>Hits</td><td>" + data.hits + "</td></tr>");
             result.append("<tr><td>Misses</td><td>" + data.misses + "</td></tr>");
             result.append("<tr><td>Turns</td><td>" + data.turns + "</td></tr>");
-            result.append("<tr><td>Avg. Time</td><td>" + data.avgTurnTime + "</td></tr>");
+            result.append("<tr><td>Avg. Turn Time</td><td>" + (data.avgTurnTime / 1000 / 60).toFixed(2)  + " minutes </td></tr>");
 
             this.$statistics.append(result);
         },
